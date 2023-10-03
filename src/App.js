d2f4f922c2cdf24902d447c63e37415ca64a2a7e
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-10-03 15:15:20 -0400

    Added initial Folder Structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PortfolioContainer from './components/PortfolioContainer';
+
+function App() {
+  return (
+    <div className="portfolio">
+      <PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
